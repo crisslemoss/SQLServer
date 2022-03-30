9f03f88b667adb3c5c1366860023caac6c0a82c7
cristiane.lemos <68552551+cristianesilvalemos@users.noreply.github.com>
2022-03-29 22:43:15 -0300

    scripts de busca dado aleatorio e script de tamanho de tabela

---
+++ TamanhoTabelas.sql
@@ -0,0 +1,14 @@
+SELECT
+    t.NAME AS Entidade,
+    p.rows AS Registros,
+    SUM(a.total_pages) * 8 AS EspacoTotalKB,
+    SUM(a.used_pages) * 8 AS EspacoUsadoKB,
+    (SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS EspacoNaoUsadoKB
+FROM sys.tables t
+INNER JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
+INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
+INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
+LEFT OUTER JOIN sys.schemas s ON t.schema_id = s.schema_id
+GROUP BY t.Name, s.Name, p.Rows
+ORDER BY Registros DESC
+
