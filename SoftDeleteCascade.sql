9f5b1f579374980c739306d83a5d84413c824d1f
cristiane.lemos <cristianelemos@soitic.com>
2022-04-04 21:08:37 -0300

    Update SoftDeleteCascade.sql

--- SoftDeleteCascade.sql
+++ SoftDeleteCascade.sql
@@ -1,90 +1,6 @@
-IF OBJECT_ID('Empresas') IS NULL 
-	CREATE TABLE Empresas (
-	  Id    INT PRIMARY KEY,
-	  Nome  VARCHAR(255),
-	  CNPJ  VARCHAR(14),	  
-	  Excluido BIT DEFAULT(0)
-	);
 
-IF OBJECT_ID('Funcionarios') IS NULL 
-BEGIN
-	CREATE TABLE Funcionarios (
-	  Id    INT PRIMARY KEY,
-	  EmpresaId INT,
-	  Nome  VARCHAR(255),
-	  CPF   VARCHAR(11),
-	  DataNascimento DATE,
-	  Excluido BIT DEFAULT(0)
-	);
-	ALTER TABLE [dbo].[Funcionarios]  
-	WITH CHECK ADD  CONSTRAINT [FK_Empresas_Funcionario] FOREIGN KEY([EmpresaId])
-	REFERENCES [dbo].[Empresas] ([Id])
-END
-
-IF OBJECT_ID('Dependentes') IS NULL 
-BEGIN
-	CREATE TABLE Dependentes (
-	  Id    INT PRIMARY KEY,
-	  FuncionarioId INT,
-	  Nome  VARCHAR(255),
-	  Parentesco VARCHAR(255),
-	  Excluido BIT DEFAULT(0)  
-	);
-	ALTER TABLE [dbo].[Dependentes]  
-	WITH CHECK ADD  CONSTRAINT [FK_Funcionarios_Dependente] FOREIGN KEY([FuncionarioId])
-	REFERENCES [dbo].[Funcionarios] ([Id])
-END
-
-
-IF OBJECT_ID('Contatos') IS NULL 
-BEGIN
-	CREATE TABLE Contatos (
-	  Id    INT PRIMARY KEY,
-	  FuncionarioId INT,
-	  Telefone VARCHAR(13) NULL,
-	  Excluido BIT DEFAULT(0)  
-	);
-	ALTER TABLE [dbo].[Contatos]  WITH CHECK ADD  CONSTRAINT [FK_Funcionarios_Contato] FOREIGN KEY([FuncionarioId])
-	REFERENCES [dbo].[Funcionarios] ([Id])
-END
-
-
-IF NOT EXISTS ( SELECT * FROM Empresas WHERE  Id < 4 ) 
-BEGIN	
-	INSERT INTO Empresas VALUES (1, 'Empresa 01','47644256000103',0);
-	INSERT INTO Empresas VALUES (2, 'Empresa 02','78896439000131',0);	
-END
-
-IF NOT EXISTS ( SELECT * FROM Funcionarios WHERE  Id < 4 ) 
-BEGIN	
-	INSERT INTO Funcionarios VALUES (1, 1,'Funcionario 01','56828259009','01/01/2000',0);
-	INSERT INTO Funcionarios VALUES (2, 1,'Funcionario 02','50720977061','02/01/2000',0);
-	INSERT INTO Funcionarios VALUES (3, 2,'Funcionario 03','79866200051','03/01/2000',0);
-END
-
-IF NOT EXISTS ( SELECT * FROM Dependentes WHERE  Id < 5 ) 
-BEGIN	
-	INSERT INTO Dependentes VALUES (1, 1,'Dependente 01','Filho',0);
-	INSERT INTO Dependentes VALUES (2, 1,'Dependente 02','Pai',0);
-	INSERT INTO Dependentes VALUES (3, 1,'Dependente 03','Irmao',0);
-
-	INSERT INTO Dependentes VALUES (4, 2,'Dependente 04','Filho',0);
-	INSERT INTO Dependentes VALUES (5, 2,'Dependente 05','Esposa',0);	
-END
-
-
-IF NOT EXISTS ( SELECT * FROM Contatos WHERE  Id < 5 ) 
-BEGIN	
-	INSERT INTO Contatos VALUES (1, 1,'(35)991111111',0);
-	INSERT INTO Contatos VALUES (2, 1,'(35)992222222',0);
-
-	INSERT INTO Contatos VALUES (3, 2,'(35)993333333',0);
-	INSERT INTO Contatos VALUES (4, 2,'(35)994444444',0);
-	INSERT INTO Contatos VALUES (5, 2,'(35)995555555',0);	
-END
-
-
-alter PROCEDURE   [dbo].[SoftDeleteCascade_rollback]
+-- Procedure que monta e executa script de exclusao lógica das tabelas em cascata a partir da tabela passada por parâmetro
+Create PROCEDURE   [dbo].[sp_SoftDeleteCascade]
   @Tabela    nvarchar(512),
   @Coluna    sysname,
   @Condicao  nvarchar(255)
@@ -117,13 +33,104 @@ BEGIN CATCH
 	ROLLBACK TRAN
 	RETURN('error')
 END CATCH
+GO
 
+-- Criacao e popula tabelas para teste da procedure a cima
+IF OBJECT_ID('Empresas') IS NULL 
+	CREATE TABLE Empresas (
+	  Id    INT PRIMARY KEY,
+	  Nome  VARCHAR(255),
+	  CNPJ  VARCHAR(14),	  
+	  Excluido BIT DEFAULT(0)
+	);
+GO
 
+IF OBJECT_ID('Funcionarios') IS NULL 
+BEGIN
+	CREATE TABLE Funcionarios (
+	  Id    INT PRIMARY KEY,
+	  EmpresaId INT,
+	  Nome  VARCHAR(255),
+	  CPF   VARCHAR(11),
+	  DataNascimento DATE,
+	  Excluido BIT DEFAULT(0)
+	);
+	ALTER TABLE [dbo].[Funcionarios]  
+	WITH CHECK ADD  CONSTRAINT [FK_Empresas_Funcionario] FOREIGN KEY([EmpresaId])
+	REFERENCES [dbo].[Empresas] ([Id])
+END
+GO
 
-exec dbo.[SoftDeleteCascade_rollback] 'empresas', 'ID', '= 1'
+IF OBJECT_ID('Dependentes') IS NULL 
+BEGIN
+	CREATE TABLE Dependentes (
+	  Id    INT PRIMARY KEY,
+	  FuncionarioId INT,
+	  Nome  VARCHAR(255),
+	  Parentesco VARCHAR(255),
+	  Excluido BIT DEFAULT(0)  
+	);
+	ALTER TABLE [dbo].[Dependentes]  
+	WITH CHECK ADD  CONSTRAINT [FK_Funcionarios_Dependente] FOREIGN KEY([FuncionarioId])
+	REFERENCES [dbo].[Funcionarios] ([Id])
+END
+GO
 
+IF OBJECT_ID('Contatos') IS NULL 
+BEGIN
+	CREATE TABLE Contatos (
+	  Id    INT PRIMARY KEY,
+	  FuncionarioId INT,
+	  Telefone VARCHAR(13) NULL,
+	  Excluido BIT DEFAULT(0)  
+	);
+	ALTER TABLE [dbo].[Contatos]  WITH CHECK ADD  CONSTRAINT [FK_Funcionarios_Contato] FOREIGN KEY([FuncionarioId])
+	REFERENCES [dbo].[Funcionarios] ([Id])
+END
+GO
 
+IF NOT EXISTS ( SELECT * FROM Empresas WHERE  Id < 4 ) 
+BEGIN	
+	INSERT INTO Empresas VALUES (1, 'Empresa 01','47644256000103',0);
+	INSERT INTO Empresas VALUES (2, 'Empresa 02','78896439000131',0);	
+END
+GO
 
+IF NOT EXISTS ( SELECT * FROM Funcionarios WHERE  Id < 4 ) 
+BEGIN	
+	INSERT INTO Funcionarios VALUES (1, 1,'Funcionario 01','56828259009','01/01/2000',0);
+	INSERT INTO Funcionarios VALUES (2, 1,'Funcionario 02','50720977061','02/01/2000',0);
+	INSERT INTO Funcionarios VALUES (3, 2,'Funcionario 03','79866200051','03/01/2000',0);
+END
+GO
+
+IF NOT EXISTS ( SELECT * FROM Dependentes WHERE  Id < 5 ) 
+BEGIN	
+	INSERT INTO Dependentes VALUES (1, 1,'Dependente 01','Filho',0);
+	INSERT INTO Dependentes VALUES (2, 1,'Dependente 02','Pai',0);
+	INSERT INTO Dependentes VALUES (3, 1,'Dependente 03','Irmao',0);
+
+	INSERT INTO Dependentes VALUES (4, 2,'Dependente 04','Filho',0);
+	INSERT INTO Dependentes VALUES (5, 2,'Dependente 05','Esposa',0);	
+END
+GO
+
+IF NOT EXISTS ( SELECT * FROM Contatos WHERE  Id < 5 ) 
+BEGIN	
+	INSERT INTO Contatos VALUES (1, 1,'(35)991111111',0);
+	INSERT INTO Contatos VALUES (2, 1,'(35)992222222',0);
+
+	INSERT INTO Contatos VALUES (3, 2,'(35)993333333',0);
+	INSERT INTO Contatos VALUES (4, 2,'(35)994444444',0);
+	INSERT INTO Contatos VALUES (5, 2,'(35)995555555',0);	
+END
+GO
+
+-- executa a exclusao da empresa de id=1
+exec dbo.[sp_SoftDeleteCascade] 'empresas', 'ID', '= 1'
+GO
+
+-- conferencia das tabelas com registros excluidos em cascata pela procedure sp_SoftDeleteCascade
 SELECT * FROM Empresas
 SELECT * FROM Funcionarios
 SELECT * FROM Dependentes
